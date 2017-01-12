e1472957052ca49e63fe44ddb8aca5e0c914355c
Wilbert van den Esker <wilbert.esker@gmail.com>
2017-01-12 11:34:32 +0100

    Added integer check

---
+++ Validation/Class1.vb
@@ -0,0 +1,25 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class validate
+
+    ''' <summary>
+    ''' Validates if input is integer
+    ''' </summary>
+    ''' <param name="value"></param>
+    ''' <returns></returns>
+    Function isInteger(value As String) As Boolean
+
+        Dim test As Integer = 0
+        If Integer.TryParse(value, test) Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+End Class
+
+
+
